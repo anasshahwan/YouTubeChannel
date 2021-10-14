a7463171fc01c8da1a51e589006484b6a3adf4b6
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2021-10-13 20:38:55 -0400

    validation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'formvalidation';
+  constructor(private fb: FormBuilder) {}
+  passwordMatched: boolean = false;
+  signUpForm = this.fb.group(
+    {
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}'),
+        ],
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(8), this.PasswordMustMatch],
+      ],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+    },
+    { validator: this.PasswordMustMatch('password', 'confirmPassword') }
+  );
+
+  PasswordMustMatch(pass: string, confirmPass: string) {
+    return (formGroup: FormGroup) => {
+      const password = formGroup.controls[pass]; // password field
+      const confirmPassword = formGroup.controls[confirmPass]; // confirmPassword field
+      // set an error if it dosnet match
+      if (password.value !== confirmPassword.value) {
+        confirmPassword.setErrors({ isPasswordMatch: false });
+        this.passwordMatched = false;
+      } else {
+        confirmPassword.setErrors(null);
+        this.passwordMatched = true;
+      }
+    };
+  }
+  onSubmit() {
+    console.log(this.signUpForm.value);
+    // this.signUpForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
